7d145e8918abe7588e719ac5641531af32d90590
keiki <sexplosive42@gmail.com>
2020-03-31 19:55:39 +0700

    Update and rename __resource.lua to fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,21 @@
+fx_version 'bodacious'
+
+game 'gta5'
+
 description 'esx_ecstasy'
 
 version '1.0.0'
 
 server_scripts {
-
-  '@es_extended/locale.lua',
-	'locales/en.lua',
+    '@es_extended/locale.lua',
+    'locales/en.lua',
     'server/main.lua',
     'config.lua'
-
 }
 
 client_scripts {
-
   '@es_extended/locale.lua',
     'locales/en.lua',
     'config.lua',
     'client/main.lua'
-
 }
