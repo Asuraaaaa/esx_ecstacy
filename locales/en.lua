b75885fb5075faa9e2cfa2b018c7764f54fc8bc9
keiki <sexplosive42@gmail.com>
2020-03-31 20:15:20 +0700

    minor cleanup

--- locales/en.lua
+++ locales/en.lua
@@ -1,5 +1,4 @@
 Locales['en'] = {
-
     ['act_imp_police'] = 'Action ~r~Impossible~s~, ~b~Police~s~: ',
     ['pickup_in_prog'] = '~y~Pickup in Progress~s~...',
     ['too_many_pills'] = 'You have too many pills',
@@ -9,10 +8,9 @@ Locales['en'] = {
     ['press_collect_ecstasy'] = 'Press ~INPUT_CONTEXT~ to buy Ecstasy Bricks',
     ['press_process_ecstasy'] = 'Press ~INPUT_CONTEXT~ to repackage Ecstasy',
     ['press_sell_ecstasy'] = 'Press ~INPUT_CONTEXT~ to sell Ecstasy Pills',
-	['inv_full_ecstasy'] = 'You cannot collect Ecstasy anymore, your inventory is ~r~full~s~',
+    ['inv_full_ecstasy'] = 'You cannot collect Ecstasy anymore, your inventory is ~r~full~s~',
     ['not_enough_bricks'] = 'You n\'do not have enough Bricks to ~r~Repackage~s~',
     ['sold_one_pill'] = 'You sold ~g~1 Pill of Ecstasy~s~',
     ['used_one_pill'] = 'You used 1 Pill ~b~of Ecstasy',
     ['need_more_money'] = 'You need more ~g~Money~s~ to buy Bricks~s~',
-
 }
