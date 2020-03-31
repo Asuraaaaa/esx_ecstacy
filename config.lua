bad41cfc2c5e74ec9e0ce23300675467d319c6e9
keiki <sexplosive42@gmail.com>
2020-03-31 20:00:27 +0700

    minor cleanup

--- config.lua
+++ config.lua
@@ -1,24 +1,24 @@
-Config              = {}
-Config.MarkerType   = 1
+Config = {}
+Config.MarkerType = 1
 Config.DrawDistance = 100.0
-Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
-Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.ZoneSize = {x = 5.0, y = 5.0, z = 3.0}
+Config.MarkerColor = {r = 100, g = 204, b = 100}
 Config.RequiredCopsEcstasy = 0
 Config.Locale = 'en'
 
 -- Ecstasy Zones
 Config.Zones = {
-    EcstasyDelivery = 		 {x=-3202.31,  y=5342.04, z=6.17},
-    EcstasyDelivery2 = 		 {x=1240.26,  y=-2950.6, z=8.32},
-    EcstasyTreatment =       {x=-1154.06, y=-1521.42, z=9.63},
-    EcstasyTreatment2 =      {x=-2173.47, y=4289.49, z=48.05},
-	EcstasyResell = 	     {x=-97.18, y=986.47,  z=234.76}
+    EcstasyDelivery = {x = -3202.31, y = 5342.04, z = 6.17},
+    EcstasyDelivery2 = {x = 1240.26, y = -2950.6, z = 8.32},
+    EcstasyTreatment = {x = -1154.06, y = -1521.42, z = 9.63},
+    EcstasyTreatment2 = {x = -2173.47, y = 4289.49, z = 48.05},
+    EcstasyResell = {x = -97.18, y = 986.47, z = 234.76}
 }
 
 Config.Map = {
-    {name="Ecstasy Delivery Site",  color=6, scale=0.8, id=403, x=-1616.82, y=2559.83, z=0.57},
-    {name="Ecstasy Delivery Site",  color=6, scale=0.8, id=403, x=1240.26,  y=-2950.6, z=8.32},
-    {name="Ecstasy Treatment",      color=7, scale=0.8, id=403, x=-1154.06, y=-1521.42, z=9.63},
-    {name="Ecstasy Treatment",  	color=7, scale=0.8, id=403, x=-2173.47, y=4289.49, z=48.05},
-    {name="Ecstasy Dealer",      	color=8, scale=0.8, id=403, x=-97.18, y=986.47,  z=234.76}
+    {name = 'Ecstasy Delivery Site', color = 6, scale = 0.8, id = 403, x = -1616.82, y = 2559.83, z = 0.57},
+    {name = 'Ecstasy Delivery Site', color = 6, scale = 0.8, id = 403, x = 1240.26,  y = -2950.6, z = 8.32},
+    {name = 'Ecstasy Treatment', color = 7, scale = 0.8, id = 403, x = -1154.06, y = -1521.42, z = 9.63},
+    {name = 'Ecstasy Treatment', color = 7, scale = 0.8, id = 403, x = -2173.47, y = 4289.49, z = 48.05},
+    {name = 'Ecstasy Dealer', color = 8, scale = 0.8, id = 403, x = -97.18, y = 986.47, z = 234.76}
   }
