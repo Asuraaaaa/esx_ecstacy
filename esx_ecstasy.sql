ac22e04df22b6f072e1c9aeea1c548aaaec6a0a6
keiki <sexplosive42@gmail.com>
2020-03-31 19:57:18 +0700

    Update esx_ecstasy.sql

--- esx_ecstasy.sql
+++ esx_ecstasy.sql
@@ -1,4 +1,4 @@
-INSERT INTO `items` (name, label) VALUES
-	('ecstasy_brick', 'Ecstasy Brick'),
-	('ecstasy_pills', 'Ecstasy Pills')
+INSERT INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('ecstasy_brick', 'Ecstasy Brick', 1, 0, 1),
+	('ecstasy_pills', 'Ecstasy Pills', 1, 0, 1)
 ;
